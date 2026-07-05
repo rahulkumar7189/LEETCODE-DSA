06762c1e2d824a630fe0124ea0c3389a097444e3
Rahul Kumar <116362511+rahulkumar7189@users.noreply.github.com>
2026-07-05 23:03:16 +0530

    Create 2509-MinimizeXor

---
+++ 2509-MinimizeXor/2509-MinimizeXor.py
@@ -0,0 +1,13 @@
+# Last updated: 7/5/2026, 11:03:16 PM
+class Solution(object):
+    def minimizeXor(self, num1, num2):
+        a, b = bin(num1).count('1'), bin(num2).count('1')
+        res = num1
+        for i in range(32):
+            if a > b and (1 << i) & num1 > 0:
+                res ^= 1 << i
+                a -= 1
+            if a < b and (1 << i) & num1 == 0:
+                res ^= 1 << i
+                a += 1
+        return res
