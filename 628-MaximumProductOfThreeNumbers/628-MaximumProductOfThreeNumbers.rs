a954e1db89836d916e5bf62d5f3fbe073022f8d0
Rahul Kumar <116362511+rahulkumar7189@users.noreply.github.com>
2026-07-29 11:40:35 +0530

    Create 628-MaximumProductOfThreeNumbers

---
+++ 628-MaximumProductOfThreeNumbers/628-MaximumProductOfThreeNumbers.rs
@@ -0,0 +1,32 @@
+// Last updated: 7/29/2026, 11:40:34 AM
+impl Solution {
+    pub fn maximum_product(nums: Vec<i32>) -> i32 {
+        let mut min1 = i32::MAX;
+        let mut min2 = i32::MAX;
+        let mut max1 = i32::MIN;
+        let mut max2 = i32::MIN;
+        let mut max3 = i32::MIN;
+
+        for x in nums {
+            if x <= min1 {
+                min2 = min1;
+                min1 = x;
+            } else if x < min2 {
+                min2 = x;
+            }
+
+            if x >= max1 {
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            } else if x > max2 {
+                max3 = max2;
+                max2 = x;
+            } else if x > max3 {
+                max3 = x;
+            }
+        }
+
+        (max1 * max2 * max3).max(min1 * min2 * max1)
+    }
+}
