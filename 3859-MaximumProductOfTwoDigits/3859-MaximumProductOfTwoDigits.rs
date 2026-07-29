f5992d095a8da5db311a21821437e893f262eb16
Rahul Kumar <116362511+rahulkumar7189@users.noreply.github.com>
2026-07-29 11:39:10 +0530

    Create 3859-MaximumProductOfTwoDigits

---
+++ 3859-MaximumProductOfTwoDigits/3859-MaximumProductOfTwoDigits.rs
@@ -0,0 +1,21 @@
+// Last updated: 7/29/2026, 11:39:10 AM
+impl Solution {
+    pub fn max_product(mut n: i32) -> i32 {
+        let mut max1 = 0;
+        let mut max2 = 0;
+
+        while n > 0 {
+            let digit = n % 10;
+            n /= 10;
+
+            if digit > max1 {
+                max2 = max1;
+                max1 = digit;
+            } else if digit > max2 {
+                max2 = digit;
+            }
+        }
+
+        max1 * max2
+    }
+}
